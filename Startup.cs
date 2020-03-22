51abf0f7088e7fb6bc03acbb5047ce74b4159ea5
Przemysław Rachwał <przemyslaw.rachwal@outlook.com>
2020-03-22 09:09:32 +0100

    add using

--- Startup.cs
+++ Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.HttpOverrides;
 
 namespace api
 {
