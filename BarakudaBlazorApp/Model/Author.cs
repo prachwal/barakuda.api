852c7c1c524b90cb14e426048260add414a19c73
Przemysław Rachwał <przemyslaw.rachwal@outlook.com>
2020-04-10 22:09:37 +0200

    new version

---
+++ BarakudaBlazorApp/Model/Author.cs
@@ -0,0 +1,7 @@
+﻿namespace Barakuda.App.Model
+{
+    public class Author
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ BarakudaBlazorApp/Model/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace Barakuda.App.Model
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public Author Author { get; set; }
+        public decimal Price { get; set; }
+    }
+}
